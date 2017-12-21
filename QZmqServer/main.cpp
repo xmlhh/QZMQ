552e9ed165def52acb384064c5a9d43ea252b3a2
oLHHo <liaohaohui521@163.com>
2017-12-21 18:24:11 +0800

    QZMQ服务端
    Signed-off-by: oLHHo <liaohaohui521@163.com>

---
+++ QZmqServer/main.cpp
@@ -0,0 +1,19 @@
+#include "LeonWgt.h"
+#include "../QBGUILIB/LCommon.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    LeonWgt w;
+
+    // 初始界面样式
+    LCommon::SetStyle("blue_1");
+    // 中文翻译
+    LCommon::SetChinese("qt_zh_CN");
+
+    w.show();
+
+    return a.exec();
+}
